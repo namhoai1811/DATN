2e7b286a15c7f885faf4e1a16c1b5e45938a3cd9
anhkhazxcv <anhkhazxcv@gmail.com>
2023-11-15 20:18:45 +0700

    4

--- API/api/src/main/java/com/datn/api/model/Hello.java
+++ API/api/src/main/java/com/datn/api/model/Hello.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @GetMapping("/hello")
     public String hello() {
-        return "Helllo";
+        return "Helllo1";
     }
 
 }
