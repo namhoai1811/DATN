b99f489cddd2281ed06ed3ddc6704683b0d809b8
nam hoai <mathoanghon18112000@gmail.com>
2023-11-28 23:48:00 +0700

    add web

---
+++ WEB/src/layout/MainLayout.js
@@ -0,0 +1,22 @@
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getInfoUser } from "../store/slice/authSlice";
+import { storage } from "../service/storage";
+
+export const MainLayout = () => {
+    // namnh
+    // const isAuthentication = !!storage.getItem("token");
+    const isAuthentication = true   ;
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if (isAuthentication) {
+            dispatch(getInfoUser());
+        }
+    }, []);
+
+    if (isAuthentication) {
+        return <Outlet />;
+    }
+    return <Navigate to="/auth/login"></Navigate>;
+};
